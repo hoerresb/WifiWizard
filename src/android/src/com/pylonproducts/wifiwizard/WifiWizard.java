19593ef686478058d6287a4f5a9bdd1ba4c3e2fe
Daniele Brugnara <daniele@brugnara.me>
2014-10-14 13:03:11 +0200

    addedd setEnableWifi, fixed some helps here and there..

--- src/android/src/com/pylonproducts/wifiwizard/WifiWizard.java
+++ src/android/src/com/pylonproducts/wifiwizard/WifiWizard.java
@@ -28,6 +28,7 @@ public class WifiWizard extends CordovaPlugin {
 	private static final String GET_SCAN_RESULTS = "getScanResults";
 	private static final String GET_CONNECTED_SSID = "getConnectedSSID";
 	private static final String IS_WIFI_ENABLED = "isWifiEnabled";
+	private static final String SET_WIFI_ENABLED = "setWifiEnabled";
 	private static final String TAG = "WifiWizard";
 
 	private WifiManager wifiManager;
@@ -45,9 +46,13 @@ public class WifiWizard extends CordovaPlugin {
 
 		this.callbackContext = callbackContext;
 
-		if (!wifiManager.isWifiEnabled()) {
-			callbackContext.error("Wifi is not enabled.");
-			return false;
+		if(action.equals(IS_WIFI_ENABLED)) {
+				return this.isWifiEnabled(callbackContext);
+		} else if(action.equals(SET_WIFI_ENABLED)) {
+				return this.setWifiEnabled(callbackContext, data);
+		} else if (!wifiManager.isWifiEnabled()) {
+				callbackContext.error("Wifi is not enabled.");
+				return false;
 		} else if(action.equals(ADD_NETWORK)) {
 				return this.addNetwork(callbackContext, data);
 		} else if(action.equals(REMOVE_NETWORK)) {
@@ -66,8 +71,6 @@ public class WifiWizard extends CordovaPlugin {
 				return this.disconnect(callbackContext);
 		} else if(action.equals(GET_CONNECTED_SSID)) {
 				return this.getConnectedSSID(callbackContext);
-		} else if(action.equals(IS_WIFI_ENABLED)) {
-				return this.isWifiEnabled(callbackContext);
 		} else {
 			callbackContext.error("Incorrect action parameter: " + action);
 		}
@@ -383,7 +386,7 @@ public class WifiWizard extends CordovaPlugin {
 	*/
 	private boolean isWifiEnabled(CallbackContext callbackContext) {
 		boolean isEnabled = wifiManager.isWifiEnabled();
-		callbackContext.success(isEnabled ? "true" : "");
+		callbackContext.success(isEnabled ? "1" : "0");
 		return isEnabled;
 	}
 
@@ -404,6 +407,34 @@ public class WifiWizard extends CordovaPlugin {
 		}
 
 		return networkId;
+	}
+
+	/**
+	 *	This method enables or disables the wifi
+	 */
+	private boolean setWifiEnabled(CallbackContext callbackContext, JSONArray data) {
+		if(!validateData(data)) {
+			callbackContext.error("WifiWizard: disconnectNetwork invalid data");
+			Log.d(TAG, "WifiWizard: disconnectNetwork invalid data");
+			return false;
+		}
+		String status = "";
+		try {
+			status = data.getString(0);
+		}
+		catch (Exception e) {
+			callbackContext.error(e.getMessage());
+			Log.d(TAG, e.getMessage());
+			return false;
+		}
+		//
+		if (wifiManager.setWifiEnabled(status.equals("true"))) {
+			callbackContext.success();
+			return true;
+		} else {
+			callbackContext.error("Cannot enable wifi");
+			return false;
+		}
 	}
 
 	private boolean validateData(JSONArray data) {
